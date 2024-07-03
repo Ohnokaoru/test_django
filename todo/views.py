201a38305c83c2a308abb5580e6f6aa28304d4aa
Kaoru <kaoru@gmail.com>
2024-07-03 13:50:05 +0800

    完成待辦事項顯示功能

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Todo
 
 
 # Create your views here.
 def todo(request):
-    return render(request, "todo/todo.html")
+    todos = None
+    if request.user.is_authenticated:
+        todos = Todo.objects.filter(user=request.user)
+        print(todos)
+    return render(request, "todo/todo.html", {"todos": todos})
